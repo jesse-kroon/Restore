022d1fe6a155feca024dbeff66baf9fa1ae9b4a6
jessekroon <jessekroon2@gmail.com>
2026-03-22 23:35:22 +0100

    add get endpoints

--- API/Program.cs
+++ API/Program.cs
@@ -14,4 +14,6 @@ var app = builder.Build();
 
 app.MapControllers();
 
+DbInitializer.InitDb(app);
+
 app.Run();
--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee17b9d14489f4a4107fdf46bf4fad0994a9b8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
