beef7e9993d2ce24a8e62b6c2882fa90facc3b67
jessekroon <jessekroon2@gmail.com>
2026-03-24 16:17:03 +0100

    add client

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+interface Product {
+  name: string,
+  price: number
+}
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+
+  const addProduct = () => {
+    setProducts(prevState => [...prevState, { name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100 }])
+  }
+
+  return (
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={() => addProduct()}>Add product</button>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default App
